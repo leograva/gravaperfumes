b319547425013e03904fa2ba69e62439a3cd0a33
Leonardo Grava de Mendonça <leonardo@MacBook-Air-de-Leonardo.local>
2026-05-08 20:47:54 -0300

    fix: CSRF_TRUSTED_ORIGINS para dominio Easypanel

--- gravaperfumes/settings.py
+++ gravaperfumes/settings.py
@@ -16,6 +16,12 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
+# CSRF trusted origins para HTTPS
+CSRF_TRUSTED_ORIGINS = os.environ.get(
+    'CSRF_TRUSTED_ORIGINS',
+    'https://gravaperfumes.h5udoo.easypanel.host'
+).split(',')
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
